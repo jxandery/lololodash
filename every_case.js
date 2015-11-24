f3a2a43c18cdf6780bd80c7d4fd3f7b8a80e66fa
Jack Yeh <jack.yeh@gmail.com>
2015-11-24 15:06:30 -0700

    pass forEach exercise

---
+++ every_case.js
@@ -0,0 +1,18 @@
+var _ = require('lodash');
+
+var worker = function(cities) {
+  return _.forEach(cities, function(city) {
+    if (city.population > 1.0) {
+      city.size = 'big';
+    }
+    else if (city.population < 0.5) {
+      city.size = 'small';
+    }
+    else {
+      city.size = 'med';
+    }
+  });
+};
+
+module.exports = worker;
+
